50053abbf46b55c292c31d7dff96b5776a8dc02d
Devin Wilson <craigvin84@yahoo.com>
2020-12-24 01:05:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
     @@all << self
   end
 
-  def self.create_from_collection(student_index_array)
+  def self.create_from_collection(students_array)
     student_index_array.each do |student_hash|
       Student.new(student_hash)
     end
